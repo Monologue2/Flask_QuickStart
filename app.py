4da9a8d040eb2204ba9f71c9b4b7927aa266472c
Monologue2 <seyoumoon@gmail.com>
2023-01-22 15:36:22 +0900

    Flask Quick Start 0.2

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask import url_for
 from markupsafe import escape
 
+#cmd : flask run // flask --app <filename> run
 app = Flask(__name__)
 
 @app.route("/")
@@ -32,8 +34,11 @@ def show_float(post_id):
 def show_subpath(subpath):
     return f'Subpath {escape(subpath)}'
 
+'''
+아래 두 URL은 다른 의미를 가진다. 큰 차이는 / 의 유무다
+이는 검색 엔진이 같은 페이지를 두 번 이상 인덱싱하는 것을 방지할 수 있다.
+'''
 
-#아래 두 URL은 다른 의미를 가진다. 큰 차이는 / 의 유무이다
 #/Project/ : (/project)로 접근할 경우 (/project/) 로 리다이렉트한다
 #파일 시스템의 폴더와 유사하다
 @app.route("/project/")
@@ -42,6 +47,31 @@ def project():
 
 #/about : (/about/)로 접근할 경우 404 Not Found Error을 return한다
 #파일 시스템의 파일의 경로와 유사하다
-@app.route('/about')
+@app.route("/about")
 def about():
     return 'The about Page'
+
+
+
+'''
+url_for()
+특정 기능을 위한 URL을 생성하려면 url_for() 함수를 사용한다
+
+url_for('route 함수 명', *변수명)
+해당 route가 가지는 url 주소와 변수명을 포함한 주소값을 return 한다.
+
+URL을 하드코딩하여 직접 구성하는 일보다 url_for을 이용하는 이유?
+    1. 더 설명하기 쉬움
+    2. URL을 수동으로 변경하지 않고 자동으로 변경 가능
+    3. 특수 문자의 이스케이프 처리
+    4. 생성 경로는 항상 절대 경로이며 상대 경로의 예상못한 동작을 방지
+    5. URL 외부에 애플리케이션이 배치될 경우 
+'''
+
+@app.route("/testname/<testname>")
+def testname(testname):
+    url_result = url_for('testname', testname = 'shogle')
+    return f'{testname}\'s files, URL:{url_result}'
+
+with app.test_request_context():
+    print(url_for('testname', testname = testname))
